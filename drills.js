173b09a846e23a4419cfccb65ed5d3eace0f0d96
Phoebe Law <phoebebasilio@gmail.com>
2019-11-14 13:59:15 -0800

    Adds 5, 7-8

--- drills.js
+++ drills.js
@@ -1,5 +1,3 @@
-
-
 //merges
 // 1. [1, 21],    26, 45,    29, 28, 2, 9,    16, 49, 39, 27, 43, 34, 46, 40;
 // 2. [1, 21],    [26, 45],    29, 28, 2, 9,    16, 49, 39, 27, 43, 34, 46, 40;
@@ -29,8 +27,7 @@
 // A: [1], [21]
 
 // Q: Which two lists would be merged on the 7th merge?
-// A: [28, 29],   [2, 9]  
-
+// A: [28, 29],   [2, 9]
 
 // 2. Understanding quicksort
 
@@ -38,7 +35,7 @@
 
 // Q: The pivot could have been 17, but could not have been 14
 // Q: The pivot could have been either 14 or 17
-// A: True, it could have been either because the partition function swaps to a position where all values that are greater than the pivot are eventually to its right. 
+// A: True, it could have been either because the partition function swaps to a position where all values that are greater than the pivot are eventually to its right.
 // Q: Neither 14 nor 17 could have been the pivot
 // Q: The pivot could have been 14, but could not have been 17
 
@@ -50,141 +47,153 @@
 // When using the first item on the list as a pivot
 // [[1, 3, 9],  [14, 17, 24, 22, 20]]
 
+"use strict";
+const LinkedList = require("./LinkedList");
 
-'use strict';
-const LinkedList = require('./LinkedList');
-    
-let stringNums = '89 30 25 32 72 70 51 42 25 24 53 55 78 50 13 40 48 32 26 2 14 33 45 72 56 44 21 88 27 68 15 62 93 98 73 28 16 46 87 28 65 38 67 16 85 63 23 69 64 91 9 70 81 27 97 82 6 88 3 7 46 13 11 64 76 31 26 38 28 13 17 69 90 1 6 7 64 43 9 73 80 98 46 27 22 87 49 83 6 39 42 51 54 84 34 53 78 40 14 5';
+let stringNums =
+	"89 30 25 32 72 70 51 42 25 24 53 55 78 50 13 40 48 32 26 2 14 33 45 72 56 44 21 88 27 68 15 62 93 98 73 28 16 46 87 28 65 38 67 16 85 63 23 69 64 91 9 70 81 27 97 82 6 88 3 7 46 13 11 64 76 31 26 38 28 13 17 69 90 1 6 7 64 43 9 73 80 98 46 27 22 87 49 83 6 39 42 51 54 84 34 53 78 40 14 5";
 
 function stringToArr(str) {
-  let arr = str.split(' ');
-  arr = arr.map(value => parseInt(value));
-  return arr;
+	let arr = str.split(" ");
+	arr = arr.map(value => parseInt(value));
+	return arr;
 }
-    
+
 let data = stringToArr(stringNums);
 
 function swap(arr, i, j) {
-  const temp = arr[i];
-  arr[i] = arr[j];
-  arr[j] = temp;
+	const temp = arr[i];
+	arr[i] = arr[j];
+	arr[j] = temp;
 }
 
 function partition(arr, start, end) {
-  let pivot = arr[end - 1];
-  let j = start;
+	let pivot = arr[end - 1];
+	let j = start;
 
-  for(let i = start; i < end - 1; i++) {
-    if(arr[i] <= pivot){
-      swap(arr, i, j);
-      j++;
-    }
-  }
-  swap(arr, end - 1, j);
-  return j;
+	for (let i = start; i < end - 1; i++) {
+		if (arr[i] <= pivot) {
+			swap(arr, i, j);
+			j++;
+		}
+	}
+	swap(arr, end - 1, j);
+	return j;
 }
 
-function quickSort(arr, start=0, end=arr.length) {
-  if(start >= end){
-    return arr;
-  }
+function quickSort(arr, start = 0, end = arr.length) {
+	if (start >= end) {
+		return arr;
+	}
 
-  let middle = partition(arr, start, end);
+	let middle = partition(arr, start, end);
 
-  arr = quickSort(arr, start, middle);
-  arr = quickSort(arr, middle + 1, end);
+	arr = quickSort(arr, start, middle);
+	arr = quickSort(arr, middle + 1, end);
 
-  return arr;
+	return arr;
 }
 
-function merge(leftArr, rightArr, arr){
-  let leftIndex = 0;
-  let rightIndex = 0;
-  let outputIndex = 0;
+function merge(leftArr, rightArr, arr) {
+	let leftIndex = 0;
+	let rightIndex = 0;
+	let outputIndex = 0;
 
-  while(leftIndex < leftArr.length && rightIndex < rightArr.length) {
-    if(leftArr[leftIndex] < rightArr[rightIndex]) {
-      arr[outputIndex++] = leftArr[leftIndex++]
-    }
-    else {
-      arr[outputIndex++] = rightArr[rightIndex++];
-    }
-  }
+	while (leftIndex < leftArr.length && rightIndex < rightArr.length) {
+		if (leftArr[leftIndex] < rightArr[rightIndex]) {
+			arr[outputIndex++] = leftArr[leftIndex++];
+		} else {
+			arr[outputIndex++] = rightArr[rightIndex++];
+		}
+	}
 
-  for(let i = leftIndex; i < leftArr.length; i++){
-    arr[outputIndex++] = leftArr[i];
-  }
-  for(let i = rightIndex; i < rightArr.length; i++){
-    arr[outputIndex++] = rightArr[i];
-  }
+	for (let i = leftIndex; i < leftArr.length; i++) {
+		arr[outputIndex++] = leftArr[i];
+	}
+	for (let i = rightIndex; i < rightArr.length; i++) {
+		arr[outputIndex++] = rightArr[i];
+	}
 
-  return arr;
+	return arr;
 }
 
 function mergeSort(arr) {
-  if(arr.length <= 1) {
-    return arr;
-  }
+	if (arr.length <= 1) {
+		return arr;
+	}
 
-  let middle = Math.floor(arr.length / 2);
-  let leftArr = arr.slice(0, middle);
-  let rightArr = arr.slice(middle, arr.length);
+	let middle = Math.floor(arr.length / 2);
+	let leftArr = arr.slice(0, middle);
+	let rightArr = arr.slice(middle, arr.length);
 
-  leftArr = mergeSort(leftArr);
-  rightArr = mergeSort(rightArr);
+	leftArr = mergeSort(leftArr);
+	rightArr = mergeSort(rightArr);
 
-  return merge(leftArr, rightArr, arr);
+	return merge(leftArr, rightArr, arr);
 }
 
 // console.log(mergeSort(data));
 
-
 function display(linkedList) {
-  console.log(JSON.stringify(linkedList));
+	console.log(JSON.stringify(linkedList));
 }
 
-function getSize(linkedList){
-  let currNode = linkedList.head;
-  let counter = 0;
-  while(currNode !== null){
-    currNode = currNode.next;
-    counter++;
-  }
+function getSize(linkedList) {
+	let currNode = linkedList.head;
+	let counter = 0;
+	while (currNode !== null) {
+		currNode = currNode.next;
+		counter++;
+	}
 
-  return counter;
+	return counter;
 }
 
-function isEmpty(linkedList){
-  if(linkedList.head === null){
-    console.log('Linked list is empty');
-    return;
+function getMiddleNode(linkedList) {
+  let middle = ll.head;
+  let double = ll.head;
+  // let indexCounter = 0;
+
+  while (double.next !== null) {
+    middle = node.next;
+    countBy1++;
+    if (double.next) double = double.next.next;
   }
-  console.log('Linked list is not empty');
+  
+  return middle;
 }
 
-function findPrevious(linkedList, value){
-  let node = linkedList.find(value);
-  let currentNode = linkedList.head;
-
-  if(node === currentNode){
-    console.log('Value is the first item of the linked list');
-    return;
-  }
-  while(currentNode.next !== node){
-    currentNode = currentNode.next;
-  }
-
-  return currentNode;
+function isEmpty(linkedList) {
+	if (linkedList.head === null) {
+		console.log("Linked list is empty");
+		return;
+	}
+	console.log("Linked list is not empty");
 }
 
-function findLast(linkedList){
-  let currentNode = linkedList.head;
+function findPrevious(linkedList, value) {
+	let node = linkedList.find(value);
+	let currentNode = linkedList.head;
 
-  while(currentNode.next !== null){
-    currentNode = currentNode.next;
-  }
+	if (node === currentNode) {
+		console.log("Value is the first item of the linked list");
+		return;
+	}
+	while (currentNode.next !== node) {
+		currentNode = currentNode.next;
+	}
 
-  return currentNode;
+	return currentNode;
+}
+
+function findLast(linkedList) {
+	let currentNode = linkedList.head;
+
+	while (currentNode.next !== null) {
+		currentNode = currentNode.next;
+	}
+
+	return currentNode;
 }
 
 let LinkedListData = new LinkedList();
@@ -192,61 +201,82 @@ data.map(value => LinkedListData.insertLast(value));
 console.log(getSize(LinkedListData));
 
 function linkedListMerge(left, right, LL) {
-  let leftNode = left.head;
-  let rightNode = right.head;
+	let leftNode = left.head;
+	let rightNode = right.head;
 
-  while(leftNode && rightNode) {
-    if(leftNode.value < rightNode.value) {
-      LL.insertLast(leftNode.value);
-      leftNode.next;
-    }
-    else{
-      LL.insertLast(rightNode.value);
-      rightNode.next;
-    }
-  }
-  while(leftNode){
-    LL.insertLast(leftNode.value);
-    leftNode.next;
-  }
-  while(rightNode){
-    LL.insertLast(rightNode.value);
-    rightNode.next;
-  }
-  display(LL);
-  return LL;
+	while (leftNode && rightNode) {
+		if (leftNode.value < rightNode.value) {
+			LL.insertLast(leftNode.value);
+			leftNode = leftNode.next;
+		} else {
+			LL.insertLast(rightNode.value);
+			rightNode = rightNode.next;
+		}
+	}
+	while (leftNode) {
+		LL.insertLast(leftNode.value);
+		leftNode = leftNode.next;
+	}
+	while (rightNode) {
+		LL.insertLast(rightNode.value);
+		rightNode = rightNode.next;
+	}
+	return LL;
 }
 
+//Can redo this using the getMiddleNode function and creating nodes for the loops instead of making a new LinkedList
 function linkedListMergeSort(LL) {
-  let length = getSize(LL);
-  let currNode = LL.head;
+	let length = getSize(LL);
+	let currNode = LL.head;
 
-  if(getSize(LL) <= 1){
-    return LL;
-  }
+	if (getSize(LL) <= 1) {
+		return LL;
+	}
 
-  let middle = Math.floor(length / 2);
-  let leftLL = new LinkedList(); 
+	let middle = Math.floor(length / 2);
+	let leftLL = new LinkedList();
 
-  for (let i = 0; i < middle; i++) {
-    leftLL.insertLast(currNode.value);
-    LL.remove(currNode.value);
-    currNode = currNode.next;
-  }
+	for (let i = 0; i < middle; i++) {
+		leftLL.insertLast(currNode.value);
+		LL.remove(currNode.value);
+		currNode = currNode.next;
+	}
 
-  leftLL = linkedListMergeSort(leftLL);
+	leftLL = linkedListMergeSort(leftLL);
 
-  let rightLL = new LinkedList();
-  for (let i = middle; i < length; i++) {
-    rightLL.insertLast(currNode.value);
-    LL.remove(currNode.value);
-    currNode = currNode.next;
-  }
+	let rightLL = new LinkedList();
+	for (let i = middle; i < length; i++) {
+		rightLL.insertLast(currNode.value);
+		LL.remove(currNode.value);
+		currNode = currNode.next;
+	}
 
-  rightLL = linkedListMergeSort(rightLL);
-  
-  return linkedListMerge(leftLL, rightLL, LL);
+	rightLL = linkedListMergeSort(rightLL);
+	return linkedListMerge(leftLL, rightLL, LL);
 }
 
 linkedListMergeSort(LinkedListData);
-display(LinkedListData);
+
+// display(LinkedListData);
+
+// function bucketSort (arr, lowest, highest) {
+// }
+
+function sortInPlace(arr) {
+  arr.forEach((item, index) => swap(arr, index, Math.floor(Math.random() * arr.length)))
+  return arr;
+}
+
+// console.log(sortInPlace([1,2,42,6,34,9]))
+
+function sortBook(arr) {
+  arr = arr.map(bookName => bookName.toLowerCase())
+  quickSort(arr)
+  return arr;
+}
+
+const books = [
+  'ouse of Representatives Speake', 'ad', 'lsdkjfsd', 'lksdjflsd', 'helolowehf', 'asdlkfj', 'Ouse of Representatives Speake'
+]
+
+// console.log(sortBook(books))
